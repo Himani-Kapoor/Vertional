3d707b8e54bf71f014f7bf3daa99cf3e760db212
Himani Kapoor <himanikapoor482@gmail.com>
2026-06-15 21:38:34 +0530

    Add vertionalworld app and update URL routing

--- vertionalhead/vertionalworld/urls.py
+++ vertionalhead/vertionalworld/urls.py
@@ -1,11 +1,6 @@
-from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from views import home
-from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', home, name='home')
-
+    path('', views.home, name='home'),
 ]
