2a00ae17825a2709b3d43d8c9936e7e769c628fe
Himani Kapoor <himanikapoor482@gmail.com>
2026-06-15 21:27:47 +0530

    Add home view and update URL routing

--- vertionalhead/vertionalworld/urls.py
+++ vertionalhead/vertionalworld/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from views import home
+from django.urls import path, include
 
 urlpatterns = [
-    # path(' ',  ),
+    path('', home, name='home')
+
 ]
