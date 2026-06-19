f98a255ddb5a8626dc9dc05ed3fafd9b77ac3bd7
Himani Kapoor <himanikapoor482@gmail.com>
2026-06-19 22:20:17 +0530

    Add login view and template, update URL routing

--- vertionalhead/vertionalworld/urls.py
+++ vertionalhead/vertionalworld/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('store/', views.store, name='store'),
+    path('login/', views.login, name='login'),
     path('userlist/', views.userList, name='userlist'),
 
 ]
--- vertionalhead/vertionalworld/views.py
+++ vertionalhead/vertionalworld/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html')
+def login(request):
+    return render(request,'login.html')
 
 def store(request):
     def logic(a,b):
